6594c9e792c9801842cc7be3de3600be0d3ee7d7
Matusz27 <mateusz.wozniak2000@gmail.com>
2021-07-24 21:03:55 +0200

    Cleaned up some code, get rid of hover from css and moved into gsap, continuing cleaning up the map animations

--- main.js
+++ main.js
@@ -4,40 +4,51 @@
 
 
 
-const cover = document.getElementById("cover");
-const buttoncover = document.getElementById("buttoncover");
 
-
-
-/* Timeline and Animation Handel */
+/* Timeline and Animation Handel For start */
 
 const repeting = { opacity: 0, y: -100, ease: "power2.out" }
 let tlstart = gsap.timeline({ defaults: repeting });
-
-
-tlstart.from("#wel", {duration: 0.5})
+let mapload = gsap.timeline({ defaults: {opacity: 0}});
+let buttonHoverAnimation = gsap.timeline();
+tlstart
+    .from("#wel", {duration: 0.5})
     .from("#to", {duration: 0.75 })
     .from("#plane", {duration: 1 })
-    .from("#buttoncover", {y: 0, scale: 0, onComplete() {addCoverEventandAnimation();}})
+    .from("#buttoncover", { y: 0, scale: 0, onComplete() {addCoverButtonEventandAnimation();}})
     .pause();
 
-function addCoverEventandAnimation() {
-    buttoncover.classList.add('buttonLoaded')
+mapload
+    .to("#cover", {duration: 0.5, display: "none" })
+    .from('.animatedMapElement', {yPercent: -100, duration: 2, ease: "elastic.out(2,0.75)"})
+    .pause();
+
+buttonHoverAnimation
+    .to('#buttoncover', { scale: 0.75, duration: 0.2, cursor: "pointer", ease: "power2.in" })
+    .pause();
+
+const buttoncover = document.getElementById("buttoncover");
+
+function addCoverButtonEventandAnimation() {
+
+    buttoncover.addEventListener("mouseover", () => {
+        buttonHoverAnimation.play();
+    })
+
+    buttoncover.addEventListener("mouseleave", () => {
+        buttonHoverAnimation.reverse();
+    })
+
     buttoncover.addEventListener("click", () => {
-        buttoncover.classList.remove('buttonLoaded')
-        gsap.to("#cover", { opacity: 0, duration: 0.5, onComplete() { 
-                cover.style.display = "none" 
-                gsap.to('.animatedMapElement', { 
-                    yPercent: 100, duration: 2,
-                    opacity: 1, ease: "elastic.out(2,0.75)"})
-            }
-        })
-    });
+        mapload.play();
+    })
 }
 
 function OnLoad() {
     tlstart.play();
 }
+
+/* Timeline and Animation Handle for city buttons */
 
 const ids = {
     "Orcius": ["Orcius-page", gsap.timeline()],
@@ -52,8 +63,7 @@ for (let [button, page] of Object.entries(ids)) {
     PageElement = document.getElementById(page[0]);
     shadowElement = PageElement.getElementsByClassName("shadow")[0]
     
-    pagetimeline = 
-        page[1]
+    page[1]
         .to(`#${page[0]} .shadow`, { opacity: 1, duration: 1, display: 'block'}, "displaycovercity")
         .to(`#${page[0]} .discription`, { opacity: 1, duration: 1, x: 0, display: 'block' }, "displaycovercity")
         .pause();
